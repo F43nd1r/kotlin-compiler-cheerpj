d467ab5ac27c0fb90832fb3c3ac544f18c8ddfe2
f43nd1r <account+github@faendir.com>
2024-04-25 17:31:21 +0200

    Working Demo

--- src/main/kotlin/com/faendir/KotlinCompiler.kt
+++ src/main/kotlin/com/faendir/KotlinCompiler.kt
@@ -4,6 +4,7 @@ import kotlin.script.experimental.api.*
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 
+@Suppress("unused")
 object KotlinCompiler {
     private val scriptCompilationConfiguration = ScriptCompilationConfiguration {
         compilerOptions("-Xuse-fast-jar-file-system=false")
@@ -11,10 +12,15 @@ object KotlinCompiler {
 
     private val scriptEvaluationConfiguration = ScriptEvaluationConfiguration()
 
-    fun eval(eval: String): Any {
+    fun eval(eval: String): Any? {
         val host = BasicJvmScriptingHost()
         return when (val result = host.eval(eval.toScriptSource(), scriptCompilationConfiguration, scriptEvaluationConfiguration)) {
-            is ResultWithDiagnostics.Success -> result.value
+            is ResultWithDiagnostics.Success -> when(val returnValue = result.value.returnValue)  {
+                is ResultValue.Value -> returnValue.value
+                is ResultValue.Error -> throw returnValue.error
+                ResultValue.NotEvaluated -> throw RuntimeException("Not evaluated")
+                is ResultValue.Unit -> Unit
+            }
             is ResultWithDiagnostics.Failure -> {
                 result.reports.filter { it.severity > ScriptDiagnostic.Severity.DEBUG }.forEach { diagnostic ->
                     println("--- Diagnostic ---")
